db673ef42f396102cdaa200fc14355ec0c61d282
maria tsekoura <maria.tsek@yahoo.gr>
2023-08-18 13:10:03 +0300

    booleans_teenager-check challenge

--- conditions-booleans/js-conditions-and-booleans_teenager-check/js/index.js
+++ conditions-booleans/js-conditions-and-booleans_teenager-check/js/index.js
@@ -6,8 +6,14 @@ button.addEventListener("click", () => {
   // Number(x) converts the string from the input to a number
   // Beware: input values are *always* strings!
   const age = Number(input.value);
+  if (age >= 13 && age <= 19) {
+    output.textContent = "You are a teen.";
+  } else {
+    output.textContent = "You are not a teen.";
+  }
+});
 
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the output.
-});
+

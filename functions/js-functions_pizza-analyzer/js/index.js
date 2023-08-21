b9e5da5ab56eaf07e5ba77c20d022c231f9f3522
maria tsekoura <maria.tsek@yahoo.gr>
2023-08-21 16:29:42 +0300

    commit  js-functions-2 challenges

--- functions/js-functions_pizza-analyzer/js/index.js
+++ functions/js-functions_pizza-analyzer/js/index.js
@@ -9,18 +9,51 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = parseFloat(pizzaInput1.value);
+  const pizzaSize2 = parseFloat(pizzaInput2.value);
+  const pizzaGain = calculatePizzaGain(pizzaSize1, pizzaSize2);
+  output.textContent = "Pizza gain: " + pizzaGain + "%";
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = parseFloat(pizzaInput1.value);
+  const pizzaSize2 = parseFloat(pizzaInput2.value);
+  const pizzaGain = calculatePizzaGain(pizzaSize1, pizzaSize2);
+  output.textContent = "Pizza gain: " + pizzaGain + "%";
 });
+
 
 // Task 1
 // define the function calculatePizzaGain here
 
+function calculatePizzaGain(diameter1, diameter2) {
+    // Calculate the radii of the pizzas
+  var radius1 = diameter1 / 2;
+  var radius2 = diameter2 / 2;
+  // Calculate the areas of the pizzas
+  var area1 = Math.PI * radius1 * radius1;
+  var area2 = Math.PI * radius2 * radius2;
+     // Calculate the difference in pizza areas
+     var pizzaGain = ((area2 - area1) / area1) * 100;
+     return Math.round(pizzaGain);
+  }
+
 // Task 2
 // define the function updatePizzaDisplay here
 
+function updatePizzaDisplay(pizzaElement, newSize) {
+  const newWidth = (newSize / 24) * 100;
+  pizzaElement.style.width = newWidth + "px";
+}
+
 // Task 3
 // define the function updateOutputColor here
+
+function updateOutputColor(size1, size2) {
+  if (size2 >= size1) {
+    outputSection.style.backgroundColor = "var(--green)";
+  } else {
+    outputSection.style.backgroundColor = "var(--red)";
+  }
+}
+

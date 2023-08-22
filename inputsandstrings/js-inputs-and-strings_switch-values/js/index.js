81428d19639f12cb51128a1e11ac264a91cfd153
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-08-22 17:07:28 +0200

    switch-values challenge

--- inputsandstrings/js-inputs-and-strings_switch-values/js/index.js
+++ inputsandstrings/js-inputs-and-strings_switch-values/js/index.js
@@ -14,13 +14,24 @@ Hint: the HTML entity for the switch button is: &updownarrow
 */
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+//-Assign the second input element to a variable
+const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+//- Assign the third button to a variable
+const switchButoon = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
-  firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+// Make sure that when the switch button is clicked, the values of the two input fields are being switched
+
+switchButoon.addEventListener("click", () => {
+  const tempValue = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = tempValue;
 });

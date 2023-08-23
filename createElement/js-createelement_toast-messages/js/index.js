86df2d16e21e5eba6d6bb949a4008bd752df4865
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-08-23 17:14:50 +0200

    toast-messages challenge

--- createElement/js-createelement_toast-messages/js/index.js
+++ createElement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,16 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
+// Exercise: Append a new entry to the toast messages container
+
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  const newToast = document.createElement("li");
+  newToast.classList.add("toast-container__message");
+  newToast.textContent = "I'm a new toast message.";
+  toastContainer.append(newToast);
 });
 
+// Exercise: Clear the stack of toast messages
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });

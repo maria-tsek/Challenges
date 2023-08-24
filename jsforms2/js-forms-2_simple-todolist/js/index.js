ed456c287305ab5574babd76c8a2cec538b07a1c
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-08-24 17:35:16 +0200

    simple todo list challenge

--- jsforms2/js-forms-2_simple-todolist/js/index.js
+++ jsforms2/js-forms-2_simple-todolist/js/index.js
@@ -2,3 +2,23 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const todoList = document.querySelector('[data-js="todoList"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  form.reset();
+  event.target.elements.headline.focus();
+  addTodoToList(data);
+});
+
+function addTodoToList(data) {
+  const listItem = document.createElement("li");
+  const headline = document.createElement("h2");
+  headline.textContent = data.headline;
+  const text = document.createElement("p");
+  text.textContent = data.task;
+  listItem.appendChild(headline);
+  listItem.appendChild(text);
+  todoList.appendChild(listItem);
+}

8fc1569f3618eadeb6b06ed0c3a8b5e284b6c84c
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-08-24 11:49:53 +0200

    input-uppercase challenge

--- jsforms2/js-forms-2_controlled-input-uppercase/js/index.js
+++ jsforms2/js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,16 @@
 console.clear();
 
+// Select the input field using its data-js attribute
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+// Add an event listener for the 'input' event using an arrow function
+input.addEventListener("input", (event) => {
+  // Get the current value of the input field
+  const inputValue = event.target.value;
+
+  // Convert the input value to uppercase and store it in a new variable
+  const uppercaseValue = inputValue.toUpperCase();
+
+  // Set the input field's value to the uppercase value
+  event.target.value = uppercaseValue;
+});

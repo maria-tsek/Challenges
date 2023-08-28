4b2edb497c23362db30f73ce2482727c3e42cf8c
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-08-28 22:53:08 +0200

    loops stars cahllenge

--- jsloops/js-loops_stars/js/index.js
+++ jsloops/js-loops_stars/js/index.js
@@ -2,13 +2,25 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars = 0) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
-  //--v-- your code here --v--
+  for (let i = 1; i <= 5; i++) {
+    const newStar = document.createElement("img");
+    if (i <= filledStars) {
+      newStar.src = "assets/star-filled.svg";
+    } else {
+      newStar.src = "assets/star-empty.svg";
+    }
 
-  //--^-- your code here --^--
+    newStar.addEventListener("click", () => {
+      console.log(i);
+      renderStars(i);
+    });
+
+    starContainer.appendChild(newStar);
+  }
 }
 
 renderStars();

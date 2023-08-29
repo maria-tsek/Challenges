a51f68387fb078bc43fa9f18d764f432b0ec1ead
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-08-29 11:34:01 +0200

    callback-1-named-vs-anonymous

--- callbackfunctions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ callbackfunctions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -1,8 +1,18 @@
 console.clear();
-
+//callback function
+function showWelcomeMessage() {
+  console.log("Welcome!You are logged in now.");
+}
+//higher-order function
 function handleUserLogin(onSuccess) {
   // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
   onSuccess();
 }
 
-// Call handleUserLogin below!
+// Call the higher-order function and pass the callback function as the first argument
+handleUserLogin(showWelcomeMessage);
+
+// Call the higher-order function without passing a callback function
+handleUserLogin(() => {
+  console.log("Welcome! You are logged in now.");
+});

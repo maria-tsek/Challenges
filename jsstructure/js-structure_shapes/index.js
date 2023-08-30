763dee8bc4fa3c53126f53b890017105b026a2f2
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-08-30 16:36:27 +0200

    structure_shapes

--- jsstructure/js-structure_shapes/index.js
+++ jsstructure/js-structure_shapes/index.js
@@ -1,23 +1,22 @@
+import { getComputedStyle } from "./utils/randomColor.js";
+import { Circle } from "./components/Circle/Circle.js";
+
 console.clear();
 
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
-});
-
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
+  square.style.backgroundColor = getComputedStyle();
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
+  pentagon.style.backgroundColor = getComputedStyle();
 });
 
-root.append(circle, square, pentagon);
+const circleElement = Circle();
+
+root.append(circleElement, square, pentagon);

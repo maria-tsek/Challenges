0113eec6bcfd34ee75abc150b7935a0dc52f235d
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-11 13:35:45 +0200

    hello-world-article2

--- react-basics/react-basics_hello-world-article/src/App.js
+++ react-basics/react-basics_hello-world-article/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return HelloWorldarticle;
+  return <HelloWorldArticle />;
 }
 
-function HelloWorldarticle() {
-  <div>
-    <article className="article">
-      <h1>Hello World</h1>
-      <p>Hello!</p>
-    </article>
-  </div>;
+function HelloWorldArticle() {
+  return (
+    <>
+      <article className="article">
+        <h1>Hello World</h1>
+        <p className="text">How are you?</p>
+      </article>
+    </>
+  );
 }

03670ed9250d9167b7a64cb9bac95ff1a5aa406e
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-11 13:07:50 +0200

    react-basics_attributes-2

--- react-basics/react-basics_attributes/src/App.js
+++ react-basics/react-basics_attributes/src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
 import "./styles.css";
+import { Article } from "./Article.js";
 
 export default function App() {
   return (
-    <div>
-      <article className="article">
-        <h2 className="article__title">Hello Honey!</h2>
-        <label htmlFor="articleInput">Label:</label>
-        <input type="text" id="articleInput" name="article_greece" />
-        <a
-          className="article__link"
-          href="https://en.wikipedia.org/wiki/Greece"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wikipedia Article
-        </a>
-      </article>
-    </div>
+    <>
+      <Article />
+    </>
   );
 }

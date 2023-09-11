5cf58d9079c15cfb77886bbfbcf5cd2619a2628a
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-11 12:18:24 +0200

    react-basics_attributes chalenge

--- react-basics/react-basics_attributes/src/App.js
+++ react-basics/react-basics_attributes/src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return (
+    <div>
+      <article className="article">
+        <h2 className="article__title">Hello Honey!</h2>
+        <label htmlFor="articleInput">Label:</label>
+        <input type="text" id="articleInput" name="article_greece" />
+        <a
+          className="article__link"
+          href="https://en.wikipedia.org/wiki/Greece"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Wikipedia Article
+        </a>
+      </article>
+    </div>
+  );
 }

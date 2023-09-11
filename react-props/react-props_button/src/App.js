2734eb48b879d2b66b7796b1d3be8e71d0bd5a42
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-11 17:18:29 +0200

    props_button challenge

--- react-props/react-props_button/src/App.js
+++ react-props/react-props_button/src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Button color="blue" disabled={false} text="Click me" />
+      <Button color="red" disabled={true} text="Disabled button" />
+    </div>
+  );
+}
+
+function Button({ color, disabled, text }) {
+  const buttonStyle = {
+    color: color,
+  };
+
+  return (
+    <button disabled={disabled} style={buttonStyle}>
+      {text}
+    </button>
+  );
 }

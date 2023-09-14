5b6684a4fc9f14ba37a7f7d0b33221e7ed56a126
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-14 12:19:06 +0200

    initial commit table-reservation

--- react-state-2/react-state-2_table-reservation/src/App.js
+++ react-state-2/react-state-2_table-reservation/src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  //  function to increment people count
+  const handleIncrement = () => {
+    setPeople(people + 1);
+  };
+
+  //  function to decrement people count
+  const handleDecrement = () => {
+    if (people > 0) {
+      setPeople(people - 1);
+    }
+  };
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        count={people}
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }

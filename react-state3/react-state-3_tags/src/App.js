c511f76c58a8eb57ee6bfff43f55dc89033a4b17
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-15 11:50:41 +0200

    initial commit tags

--- react-state3/react-state-3_tags/src/App.js
+++ react-state3/react-state-3_tags/src/App.js
@@ -6,10 +6,22 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    // a copy of the current tags array and the new tag
+    const newTags = [...tags, newTag];
+    setTags(newTags);
+  }
+  function handleDeleteTag(tagToDelete) {
+    // Filter out the tag to delete from
+    const newTags = tags.filter((tag) => tag !== tagToDelete);
+    // Update the state with the new tags array
+    setTags(newTags);
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }

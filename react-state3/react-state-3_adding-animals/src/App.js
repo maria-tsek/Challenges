b91ab3a6acdb08d11fec0818a025cb7151045d1e
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-15 11:25:44 +0200

    initial commit adding-animals

--- react-state3/react-state-3_adding-animals/src/App.js
+++ react-state3/react-state-3_adding-animals/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { uid } from "uid";
 import "./styles.css";
 import Form from "./components/Form/index.js";
 import List from "./components/List/index.js";
@@ -25,7 +26,10 @@ export default function App() {
   const [animals, setAnimals] = useState(initialAnimals);
 
   function handleAddAnimal(newAnimal) {
-    console.log(newAnimal);
+    setAnimals((prevAnimals) => [
+      ...prevAnimals,
+      { id: uid(), ...newAnimal }, // Add a unique ID to the new animal
+    ]);
   }
 
   return (

41fe96b92052fdb9a64fb4b019a0b2b541e78232
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-17 19:29:52 +0200

    initial commit fetch_iss-tracker

--- react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.js
+++ react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
-
+import { useEffect } from "react";
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function App() {
@@ -11,7 +11,38 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  // Define the function to fetch ISS coordinates
+  const getISSCoords = async () => {
+    try {
+      const response = await fetch(URL);
+      if (response.ok) {
+        const data = await response.json();
+        setCoords({
+          longitude: data.longitude,
+          latitude: data.latitude,
+        });
+      } else {
+        throw new Error("Failed to fetch ISS coordinates");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // the useEffect hook to fetch ISS coordinates on component mount
+  useEffect(() => {
+    getISSCoords();
+  }, []); // empty dependency array to run the effect only once on mount
+
+  // refresh ISS coordinates every 5 seconds
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      getISSCoords();
+    }, 5000);
+
+    // return a cleanup function to clear the interval
+    return () => clearInterval(intervalId);
+  }, []); // empty dependency array to run the effect only once on mount
 
   return (
     <main>

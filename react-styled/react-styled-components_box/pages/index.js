ab7cf79920841a61f3dc70ea9328225f4f781528
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-25 13:23:17 +0200

    initial commit styled-components_box

--- react-styled/react-styled-components_box/pages/index.js
+++ react-styled/react-styled-components_box/pages/index.js
@@ -1,10 +1,15 @@
+import Box from "../components/BoxWithClassName/BoxWithStyledComponents.js";
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
-
+import styled from "styled-components";
 export default function HomePage() {
+  const Container = styled.div`
+    display: flex;
+  `;
   return (
-    <div>
+    <Container>
       <BoxWithClassName />
-      <BoxWithClassName isBlack />
-    </div>
+      <Box isBlack={true} />
+      <Box />
+    </Container>
   );
 }

cf8070aec667f27950196451904651155284071f
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-09-26 17:26:35 +0200

    initial commit iss-tracker

--- react-data-fetching/react-data-fetching_iss-tracker/pages/_app.js
+++ react-data-fetching/react-data-fetching_iss-tracker/pages/_app.js
@@ -1,10 +1,15 @@
+import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher, refreshInterval: 5000 }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }

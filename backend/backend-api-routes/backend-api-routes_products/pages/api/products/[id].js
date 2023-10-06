7d070fe480cd120b395cc14571213e804fe4bf1f
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-10-06 15:25:00 +0200

    initial commit backend-api-routes_products

---
+++ backend/backend-api-routes/backend-api-routes_products/pages/api/products/[id].js
@@ -0,0 +1,11 @@
+import { getProductById } from "@/services/productServices";
+
+export default function handler(request, response) {
+  const { id } = request.query;
+  const product = getProductById(id);
+
+  if (!product) {
+    return response.status(404).json({ status: "Id not found" });
+  }
+  response.status(200).json(product);
+}
---
+++ backend/backend-api-routes/backend-api-routes_products/pages/products/index.js
@@ -0,0 +1,29 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+function ProductsPage() {
+  const { data, error } = useSWR("/api/products", fetcher);
+
+  if (error) return <div>Error loading data</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {data.map((product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+            <p>{product.currency}</p>
+            <p>{product.category}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductsPage;
